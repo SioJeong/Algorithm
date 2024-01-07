f822e10e10c57696c622b5c806491fb21849e490
jeongsangyoung <155740369+SioJeong@users.noreply.github.com>
2024-01-07 17:25:04 +0900

    Add files via upload
    test

---
+++ Algorithm/Baekjoon/src/step1/_1_2557.java
@@ -0,0 +1,6 @@
+@SuppressWarnings("ALL")
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Algorithm/Baekjoon/src/step1/_2_1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+@SuppressWarnings("ALL")
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B = 0;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        System.out.println(A + B);
+    }
+}
