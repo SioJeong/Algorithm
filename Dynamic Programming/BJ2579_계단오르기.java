bec83091429127caa022c9c250a526c554370b0e
jeongsangyoung <155740369+SioJeong@users.noreply.github.com>
2024-04-14 21:35:47 +0900

    4/14 DP / 반복문으로 리팩토링

--- Dynamic Programming/BJ2579_계단오르기.java
+++ Dynamic Programming/BJ2579_계단오르기.java
@@ -1,15 +1,12 @@
 import java.util.*;
 
 public class BJ2579_계단오르기 {
-	
-	static int[] points, dp;
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		points = new int[n+1];
-		dp = new int[n+1];
+		int[] points = new int[n+1];
+		int[] dp = new int[n+1];
 		
 		for(int i = 1; i <= n; i++) {
 			points[i] = sc.nextInt();
@@ -17,18 +14,24 @@ public class BJ2579_계단오르기 {
 		
 		dp[1] = points[1];
 		
-		if(n > 1) dp[2] = points[1] + points[2];
-		
-		System.out.println(find(n));
-	}
-
-	private static int find(int idx) {
-		if(idx <= 2) return dp[idx];
-		
-		if(dp[idx] == 0) {
-			dp[idx] = Math.max(find(idx-2), find(idx-3) + points[idx-1]) + points[idx];
+		if(n >= 2) {
+			dp[2] = points[1] + points[2];
+			
+			for(int i = 3; i <= n; i++) {
+				dp[i] = Math.max(dp[i-2], dp[i-3] + points[i-1]) + points[i];
+			}
 		}
 		
-		return dp[idx];
+		System.out.println(dp[n]);
 	}
+	
+//	private static int find(int idx) {
+//		if(idx <= 2) return dp[idx];
+//		
+//		if(dp[idx] == 0) {
+//			dp[idx] = Math.max(find(idx-2), find(idx-3) + points[idx-1]) + points[idx];
+//		}
+//		
+//		return dp[idx];
+//	}
 }
